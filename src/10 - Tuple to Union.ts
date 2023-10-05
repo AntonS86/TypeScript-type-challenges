2bfd49a144a4da96dd1fa9836487771d7502d27c
Anton Stepanov <37770694+AntonS86@users.noreply.github.com>
2023-10-06 01:01:14 +0300

    Update 10 - Tuple to Union.ts

--- src/10 - Tuple to Union.ts
+++ src/10 - Tuple to Union.ts
@@ -20,7 +20,17 @@
 
 /* _____________ Your Code Here _____________ */
 
-type TupleToUnion<T extends any[]> = T[number];
+type TupleToUnion1<T extends unknown[]> = T extends [infer F, ...infer Tail] ? F | TupleToUnion<Tail> : never;
+
+type TupleToUnion2<T extends unknown[]> = T[number];
+
+type TupleToUnion3<T> = T[number & keyof T];
+
+type TupleToUnion4<T> = T[number & symbol & string];
+
+type TupleToUnio5<T> = T[Extract<keyof T, number>];
+
+type TupleToUnion<T> = T extends (infer R)[] ? R : never;
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
